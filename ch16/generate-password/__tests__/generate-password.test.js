482fb3a3c2d132a9c277605560d52eb80592f368
John D. Aynedjian <johndavid5@yahoo.com>
2018-03-07 22:39:32 -0500

    Oh, boy!  My first jest unit test!

---
+++ ch16/generate-password/__tests__/generate-password.test.js
@@ -0,0 +1,13 @@
+// "Ah-hahh...this kid's a real mock..."
+jest.dontMock('../generate-password.js');
+
+describe('method generatePassword', ()=>{
+	let password;
+	generatePassword = require('../generate-password');
+	// it = "individual test"...?
+	it('returns a generated password of lower-case characters and numbers with the length of 8', (done)=>{
+		password = generatePassword();
+		expect(password).toMatch(/^[a-z0-9]{8}$/);
+		done();
+	});
+});
---
+++ ch16/generate-password/runit.js
@@ -0,0 +1,3 @@
+var generate_password=require('./generate-password.js');
+console.log("generate_password()=", generate_password());
+console.log("Let off some steam, Bennett!");
